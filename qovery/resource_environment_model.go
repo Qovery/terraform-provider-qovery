3a1daa5759a44716f42c54064e6638cc4cc607d6
Bilel Benamira <bbenamira@qovery.com>
2022-02-15 20:02:35 +0100

    chore: refactor environment to use model

---
+++ qovery/resource_environment_model.go
@@ -0,0 +1,38 @@
+package qovery
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/qovery/qovery-client-go"
+)
+
+type Environment struct {
+	Id        types.String `tfsdk:"id"`
+	ProjectId types.String `tfsdk:"project_id"`
+	ClusterId types.String `tfsdk:"cluster_id"`
+	Name      types.String `tfsdk:"name"`
+	Mode      types.String `tfsdk:"mode"`
+}
+
+func (e Environment) toCreateEnvironmentRequest() qovery.EnvironmentRequest {
+	return qovery.EnvironmentRequest{
+		Name:    toString(e.Name),
+		Cluster: toStringPointer(e.ClusterId),
+		Mode:    toStringPointer(e.Mode),
+	}
+}
+
+func (e Environment) toUpdateEnvironmentRequest() qovery.EnvironmentEditRequest {
+	return qovery.EnvironmentEditRequest{
+		Name: toStringPointer(e.Name),
+	}
+}
+
+func convertResponseToEnvironment(environment *qovery.EnvironmentResponse) Environment {
+	return Environment{
+		Id:        fromString(environment.Id),
+		ProjectId: fromString(environment.Project.Id),
+		ClusterId: fromString(environment.ClusterId),
+		Name:      fromString(environment.Name),
+		Mode:      fromString(environment.Mode),
+	}
+}
