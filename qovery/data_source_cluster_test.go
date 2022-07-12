c7c45424042d8f27ba5026ef77d491b75d27f7f5
Bilel Benamira <bbenamira@qovery.com>
2022-07-12 11:25:52 +0200

    tests: fix cluster datasource test

--- qovery/data_source_cluster_test.go
+++ qovery/data_source_cluster_test.go
@@ -28,7 +28,7 @@ func TestAcc_ClusterDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr("data.qovery_cluster.test", "cloud_provider", "AWS"),
 					resource.TestCheckResourceAttr("data.qovery_cluster.test", "region", "eu-west-3"),
 					resource.TestCheckResourceAttr("data.qovery_cluster.test", "kubernetes_mode", "MANAGED"),
-					resource.TestCheckResourceAttr("data.qovery_cluster.test", "instance_type", "T3A_LARGE"),
+					resource.TestCheckResourceAttr("data.qovery_cluster.test", "instance_type", "T3A_XLARGE"),
 					resource.TestCheckResourceAttr("data.qovery_cluster.test", "min_running_nodes", "3"),
 					resource.TestCheckResourceAttr("data.qovery_cluster.test", "max_running_nodes", "30"),
 					resource.TestCheckNoResourceAttr("data.qovery_cluster.test", "routing_table.0"),
