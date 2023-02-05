877c9728c06ad6acae720e378691f1e2faeb83ed
acarranoqovery <105300721+acarranoqovery@users.noreply.github.com>
2023-02-05 11:45:26 +0100

    Update resource.tf

--- examples/resources/qovery_container_registry/resource.tf
+++ examples/resources/qovery_container_registry/resource.tf
@@ -4,12 +4,12 @@ resource "qovery_container_registry" "my_container_registry" {
   name            = "my_aws_creds"
   kind            = "DOCKER_HUB"
   url             = "https://docker.io"
+
+  # Optional
   config = {
     username = "<my_username>"
     password = "<my_password>"
   }
-
-  # Optional
   description = "My Docker Hub Registry"
 
   depends_on = [
