66b902e07f25d8e06dc50cc28e2ce5783f83c37b
Melvin Zottola <mzottola@qovery.com>
2023-09-06 12:23:15 +0200

    fix: documentation variable overrides (#280)

--- examples/resources/qovery_container/resource.tf
+++ examples/resources/qovery_container/resource.tf
@@ -59,7 +59,7 @@ resource "qovery_container" "my_container" {
   ]
   environment_variable_overrides = [
     {
-      # the key of the override must be the name of the aliased variable
+      # the key of the override must be the name of the overridden variable
       # e.g here it is an override on a variable declared at project scope "SOME_PROJECT_VARIABLE"
       key   = "SOME_PROJECT_VARIABLE"
       value = "OVERRIDDEN_VALUE"
@@ -81,7 +81,7 @@ resource "qovery_container" "my_container" {
   ]
   secret_overrides = [
     {
-      # the key of the override must be the name of the aliased secret
+      # the key of the override must be the name of the overridden secret
       # e.g here it is an override on a secret declared at project scope "SOME_PROJECT_SECRET"
       key   = "SOME_PROJECT_SECRET"
       value = "OVERRIDDEN_VALUE"
