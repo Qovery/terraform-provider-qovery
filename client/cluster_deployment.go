d2381da44f272f5f0e8c9641a223c02ba2ea761c
Melvin Zottola <mzottola@qovery.com>
2023-10-02 16:46:05 +0200

    feat: Bump go client

--- client/cluster_deployment.go
+++ client/cluster_deployment.go
@@ -8,7 +8,7 @@ import (
 	"github.com/qovery/terraform-provider-qovery/client/apierrors"
 )
 
-func (c *Client) deployCluster(ctx context.Context, organizationID string, cluster *qovery.Cluster) (*qovery.StateEnum, *apierrors.APIError) {
+func (c *Client) deployCluster(ctx context.Context, organizationID string, cluster *qovery.Cluster) (*qovery.ClusterStateEnum, *apierrors.APIError) {
 	// deploy cluster even if it is already in the DEPLOYED state to apply any changed done with editCluster
 	_, res, err := c.api.ClustersApi.
 		DeployCluster(ctx, organizationID, cluster.Id).
@@ -17,7 +17,7 @@ func (c *Client) deployCluster(ctx context.Context, organizationID string, clust
 		return nil, apierrors.NewDeployError(apierrors.APIResourceCluster, cluster.Id, res, err)
 	}
 
-	statusChecker := newClusterStatusCheckerWaitFunc(c, organizationID, cluster.Id, qovery.STATEENUM_DEPLOYED)
+	statusChecker := newClusterStatusCheckerWaitFunc(c, organizationID, cluster.Id, qovery.CLUSTERSTATEENUM_DEPLOYED)
 	if apiErr := wait(ctx, statusChecker, nil); apiErr != nil {
 		return nil, apiErr
 	}
@@ -28,15 +28,15 @@ func (c *Client) deployCluster(ctx context.Context, organizationID string, clust
 	return clusterStatus.Status, nil
 }
 
-func (c *Client) stopCluster(ctx context.Context, organizationID string, cluster *qovery.Cluster) (*qovery.StateEnum, *apierrors.APIError) {
+func (c *Client) stopCluster(ctx context.Context, organizationID string, cluster *qovery.Cluster) (*qovery.ClusterStateEnum, *apierrors.APIError) {
 	status, apiErr := c.getClusterStatus(ctx, organizationID, cluster.Id)
 	if apiErr != nil {
 		return nil, apiErr
 	}
 
 	switch status.GetStatus() {
-	case qovery.STATEENUM_STOPPED, qovery.STATEENUM_READY:
-		status := qovery.STATEENUM_STOPPED
+	case qovery.CLUSTERSTATEENUM_STOPPED, qovery.CLUSTERSTATEENUM_READY:
+		status := qovery.CLUSTERSTATEENUM_STOPPED
 		return &status, nil
 	default:
 		_, res, err := c.api.ClustersApi.
@@ -47,7 +47,7 @@ func (c *Client) stopCluster(ctx context.Context, organizationID string, cluster
 		}
 	}
 
-	statusChecker := newClusterStatusCheckerWaitFunc(c, organizationID, cluster.Id, qovery.STATEENUM_STOPPED)
+	statusChecker := newClusterStatusCheckerWaitFunc(c, organizationID, cluster.Id, qovery.CLUSTERSTATEENUM_STOPPED)
 	if apiErr := wait(ctx, statusChecker, nil); apiErr != nil {
 		return nil, apiErr
 	}
