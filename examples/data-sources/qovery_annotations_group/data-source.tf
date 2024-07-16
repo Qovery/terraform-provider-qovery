4535f7b538bf220a4659e2537f0fa152cd7a8fdd
Pierre Gerbelot <pgerbelot@qovery.com>
2024-07-16 13:51:22 +0200

    fix: add documentations for helm_repository, labels_group and annotations group data-sources

---
+++ examples/data-sources/qovery_annotations_group/data-source.tf
@@ -0,0 +1,4 @@
+data "qovery_annotations_group" "my_annotations_group" {
+  id              = "<annotations_group_id>"
+  organization_id = "<organization_id>"
+}
---
+++ examples/data-sources/qovery_helm_repository/data-source.tf
@@ -0,0 +1,4 @@
+data "qovery_helm_repository" "my_helm_repository" {
+  id              = "<helm_repository_id>"
+  organization_id = "<organization_id>"
+}
