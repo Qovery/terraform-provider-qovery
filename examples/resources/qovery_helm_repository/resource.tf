3e74519e3447c41a449af0e394ea2f531e913b09
Gagnaire Charles-Edouard <baalooos@hotmail.com>
2024-07-31 10:39:29 +0200

    fix documentation for qovery_helm_repository (#419)
    * fix documentation for qovery_helm_repository
    
    * run task docs
    
    ---------
    
    Co-authored-by: Charles-Edouard <baalooos@Charles-Edouards-Laptop.local>

--- examples/resources/qovery_helm_repository/resource.tf
+++ examples/resources/qovery_helm_repository/resource.tf
@@ -1,10 +1,10 @@
 resource "qovery_helm_repository" "my_helm_repository" {
   # Required
-  organization_id      = qovery_organization.my_organization.id
-  name                 = "my_helm_repository"
-  kind                 = "OCI_DOCKER_HUB"
-  url                  = "https://docker.io"
-  skip_tls_certificate = false
+  organization_id       = qovery_organization.my_organization.id
+  name                  = "my_helm_repository"
+  kind                  = "OCI_DOCKER_HUB"
+  url                   = "https://docker.io"
+  skip_tls_verification = false
 
   # Optional
   description = "My Helm repository"
