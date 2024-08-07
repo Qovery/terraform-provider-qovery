1a7cc5f29840e31a0c3660ae0eee78fe9277a8d0
Fabien FLEUREAU <ffleureau@qovery.com>
2024-08-07 15:31:39 +0200

    feat(cor-1017): update api for credentials

--- internal/infrastructure/repositories/qoveryapi/credentials_qoveryapi_models.go
+++ internal/infrastructure/repositories/qoveryapi/credentials_qoveryapi_models.go
@@ -1,8 +1,8 @@
 package qoveryapi
 
 import (
+	"github.com/pkg/errors"
 	"github.com/qovery/qovery-client-go"
-
 	"github.com/qovery/terraform-provider-qovery/internal/domain/credentials"
 )
 
@@ -11,12 +11,28 @@ func newDomainCredentialsFromQovery(organizationID string, creds *qovery.Cluster
 	if creds == nil {
 		return nil, credentials.ErrNilCredentials
 	}
-
-	return credentials.NewCredentials(credentials.NewCredentialsParams{
-		CredentialsID:  creds.GetId(),
-		OrganizationID: organizationID,
-		Name:           creds.GetName(),
-	})
+	switch castedCreds := creds.GetActualInstance().(type) {
+	case *qovery.AwsClusterCredentials:
+		return credentials.NewCredentials(credentials.NewCredentialsParams{
+			CredentialsID:  castedCreds.GetId(),
+			OrganizationID: organizationID,
+			Name:           castedCreds.GetName(),
+		})
+	case *qovery.ScalewayClusterCredentials:
+		return credentials.NewCredentials(credentials.NewCredentialsParams{
+			CredentialsID:  castedCreds.GetId(),
+			OrganizationID: organizationID,
+			Name:           castedCreds.GetName(),
+		})
+	case *qovery.GenericClusterCredentials:
+		return credentials.NewCredentials(credentials.NewCredentialsParams{
+			CredentialsID:  castedCreds.GetId(),
+			OrganizationID: organizationID,
+			Name:           castedCreds.GetName(),
+		})
+	default:
+		return nil, errors.New("unknown credentials type")
+	}
 }
 
 // newQoveryAwsCredentialsRequestFromDomain takes the domain request credentials.UpsertAwsRequest and turns it into a qovery.AwsCredentialsRequest to make the api call.
