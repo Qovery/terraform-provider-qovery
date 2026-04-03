26435e485fdf6d60a00a1c1ac8a39f18924affed
Guillaume Dubroeucq <gdubroeucq@qovery.com>
2026-04-03 15:37:01 +0200

    feat(QOV-1764): add EnvironmentVariableFile and SecretFile terraform model types

---
+++ qovery/environment_variable_file_model.go
@@ -0,0 +1,319 @@
+package qovery
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/qovery/qovery-client-go"
+
+	"github.com/qovery/terraform-provider-qovery/client"
+	"github.com/qovery/terraform-provider-qovery/internal/domain/variable"
+)
+
+var environmentVariableFileAttrTypes = map[string]attr.Type{
+	"id":          types.StringType,
+	"key":         types.StringType,
+	"value":       types.StringType,
+	"mount_path":  types.StringType,
+	"description": types.StringType,
+}
+
+type EnvironmentVariableFileList []EnvironmentVariableFile
+
+func (vars EnvironmentVariableFileList) toTerraformSet(ctx context.Context) types.Set {
+	var environmentVariableFileObjectType = types.ObjectType{
+		AttrTypes: environmentVariableFileAttrTypes,
+	}
+	if vars == nil {
+		return types.SetNull(environmentVariableFileObjectType)
+	}
+
+	var elements = make([]attr.Value, 0, len(vars))
+	for _, v := range vars {
+		elements = append(elements, v.toTerraformObject())
+	}
+	set, diagnostics := types.SetValueFrom(ctx, environmentVariableFileObjectType, elements)
+	if diagnostics.HasError() {
+		panic("TODO")
+	}
+	return set
+}
+
+func (vars EnvironmentVariableFileList) contains(e EnvironmentVariableFile) bool {
+	for _, v := range vars {
+		if e.Key == v.Key {
+			return true
+		}
+	}
+	return false
+}
+
+func (vars EnvironmentVariableFileList) find(key string) *EnvironmentVariableFile {
+	for _, v := range vars {
+		if v.Key.ValueString() == key {
+			return &v
+		}
+	}
+	return nil
+}
+
+func (vars EnvironmentVariableFileList) diffRequest(old EnvironmentVariableFileList) variable.DiffRequest {
+	diff := variable.DiffRequest{
+		Create: []variable.DiffCreateRequest{},
+		Update: []variable.DiffUpdateRequest{},
+		Delete: []variable.DiffDeleteRequest{},
+	}
+
+	for _, e := range old {
+		if updatedVar := vars.find(ToString(e.Key)); updatedVar != nil {
+			if updatedVar.MountPath != e.MountPath {
+				// mount_path changed — delete + recreate
+				diff.Delete = append(diff.Delete, e.toDiffDeleteRequest())
+				diff.Create = append(diff.Create, updatedVar.toDiffCreateRequest())
+			} else if updatedVar.Value != e.Value || updatedVar.Description != e.Description {
+				diff.Update = append(diff.Update, e.toDiffUpdateRequest(*updatedVar))
+			}
+		} else {
+			diff.Delete = append(diff.Delete, e.toDiffDeleteRequest())
+		}
+	}
+
+	for _, e := range vars {
+		if !old.contains(e) {
+			diff.Create = append(diff.Create, e.toDiffCreateRequest())
+		}
+	}
+
+	return diff
+}
+
+func (vars EnvironmentVariableFileList) diff(old EnvironmentVariableFileList) client.EnvironmentVariablesDiff {
+	diff := client.EnvironmentVariablesDiff{
+		Create: []client.EnvironmentVariableCreateRequest{},
+		Update: []client.EnvironmentVariableUpdateRequest{},
+		Delete: []client.EnvironmentVariableDeleteRequest{},
+	}
+
+	for _, e := range old {
+		if updatedVar := vars.find(ToString(e.Key)); updatedVar != nil {
+			if updatedVar.MountPath != e.MountPath {
+				// mount_path changed — delete + recreate
+				diff.Delete = append(diff.Delete, client.EnvironmentVariableDeleteRequest{Id: ToString(e.Id)})
+				diff.Create = append(diff.Create, updatedVar.toCreateRequest())
+			} else if updatedVar.Value != e.Value || updatedVar.Description != e.Description {
+				diff.Update = append(diff.Update, e.toUpdateRequest(*updatedVar))
+			}
+		} else {
+			diff.Delete = append(diff.Delete, client.EnvironmentVariableDeleteRequest{Id: ToString(e.Id)})
+		}
+	}
+
+	for _, e := range vars {
+		if !old.contains(e) {
+			diff.Create = append(diff.Create, e.toCreateRequest())
+		}
+	}
+
+	return diff
+}
+
+type EnvironmentVariableFile struct {
+	Id          types.String `tfsdk:"id"`
+	Key         types.String `tfsdk:"key"`
+	Value       types.String `tfsdk:"value"`
+	MountPath   types.String `tfsdk:"mount_path"`
+	Description types.String `tfsdk:"description"`
+}
+
+func (e EnvironmentVariableFile) toTerraformObject() types.Object {
+	var attributes = map[string]attr.Value{
+		"id":          e.Id,
+		"key":         e.Key,
+		"value":       e.Value,
+		"mount_path":  e.MountPath,
+		"description": e.Description,
+	}
+	terraformObjectValue, diagnostics := types.ObjectValue(environmentVariableFileAttrTypes, attributes)
+	if diagnostics.HasError() {
+		panic("TODO")
+	}
+	return terraformObjectValue
+}
+
+func (e EnvironmentVariableFile) toCreateRequest() client.EnvironmentVariableCreateRequest {
+	req := client.EnvironmentVariableCreateRequest{
+		EnvironmentVariableRequest: qovery.EnvironmentVariableRequest{
+			Key:         ToString(e.Key),
+			Value:       ToStringPointer(e.Value),
+			Description: *qovery.NewNullableString(ToStringPointer(e.Description)),
+		},
+	}
+	mountPath := ToString(e.MountPath)
+	if mountPath != "" {
+		req.EnvironmentVariableRequest.MountPath = *qovery.NewNullableString(&mountPath)
+	}
+	return req
+}
+
+func (e EnvironmentVariableFile) toDiffCreateRequest() variable.DiffCreateRequest {
+	return variable.DiffCreateRequest{
+		UpsertRequest: variable.UpsertRequest{
+			Key:         ToString(e.Key),
+			Value:       ToString(e.Value),
+			Description: ToString(e.Description),
+			MountPath:   ToString(e.MountPath),
+		},
+	}
+}
+
+func (e EnvironmentVariableFile) toUpdateRequest(new EnvironmentVariableFile) client.EnvironmentVariableUpdateRequest {
+	req := client.EnvironmentVariableUpdateRequest{
+		Id: ToString(e.Id),
+		EnvironmentVariableEditRequest: qovery.EnvironmentVariableEditRequest{
+			Key:         ToString(e.Key),
+			Value:       ToStringPointer(new.Value),
+			Description: *qovery.NewNullableString(ToStringPointer(new.Description)),
+		},
+	}
+	mountPath := ToString(e.MountPath)
+	if mountPath != "" {
+		req.EnvironmentVariableEditRequest.MountPath = *qovery.NewNullableString(&mountPath)
+	}
+	return req
+}
+
+func (e EnvironmentVariableFile) toDiffUpdateRequest(new EnvironmentVariableFile) variable.DiffUpdateRequest {
+	return variable.DiffUpdateRequest{
+		VariableID: ToString(e.Id),
+		UpsertRequest: variable.UpsertRequest{
+			Key:         ToString(e.Key),
+			Value:       ToString(new.Value),
+			Description: ToString(new.Description),
+			MountPath:   ToString(e.MountPath),
+		},
+	}
+}
+
+func (e EnvironmentVariableFile) toDiffDeleteRequest() variable.DiffDeleteRequest {
+	return variable.DiffDeleteRequest{
+		VariableID: ToString(e.Id),
+	}
+}
+
+func toEnvironmentVariableFile(v types.Object) EnvironmentVariableFile {
+	return EnvironmentVariableFile{
+		Id:          v.Attributes()["id"].(types.String),
+		Key:         v.Attributes()["key"].(types.String),
+		Value:       v.Attributes()["value"].(types.String),
+		MountPath:   v.Attributes()["mount_path"].(types.String),
+		Description: v.Attributes()["description"].(types.String),
+	}
+}
+
+func toEnvironmentVariableFileList(vars types.Set) EnvironmentVariableFileList {
+	if vars.IsNull() || vars.IsUnknown() {
+		return nil
+	}
+
+	environmentVariableFiles := make([]EnvironmentVariableFile, 0, len(vars.Elements()))
+	for _, elem := range vars.Elements() {
+		environmentVariableFiles = append(environmentVariableFiles, toEnvironmentVariableFile(elem.(types.Object)))
+	}
+
+	return environmentVariableFiles
+}
+
+func convertDomainVariablesToEnvironmentVariableFileList(vars variable.Variables, scope variable.Scope, variableType string) EnvironmentVariableFileList {
+	list := make([]EnvironmentVariableFile, 0, len(vars))
+	for _, v := range vars {
+		if v.Scope != scope || v.Type != variableType {
+			continue
+		}
+		list = append(list, convertDomainVariableToEnvironmentVariableFile(v, nil))
+	}
+
+	if len(list) == 0 {
+		return nil
+	}
+	return list
+}
+
+func convertDomainVariablesToEnvironmentVariableFileListWithNullableInitialState(ctx context.Context, initialState types.Set, vars variable.Variables, scope variable.Scope, variableType string) EnvironmentVariableFileList {
+	list := make([]EnvironmentVariableFile, 0, len(vars))
+	variableMapByKey := buildVariableFileMap(ctx, initialState)
+
+	for _, v := range vars {
+		if v.Scope != scope || v.Type != variableType {
+			continue
+		}
+		currentVariable := variableMapByKey[v.Key]
+		list = append(list, convertDomainVariableToEnvironmentVariableFile(v, &currentVariable))
+	}
+
+	// Return nil only if list is empty and original state is nil
+	if len(list) == 0 && initialState.IsNull() {
+		return nil
+	}
+	// Otherwise return the list, even empty (`[]` in the terraform file)
+	return list
+}
+
+func buildVariableFileMap(ctx context.Context, initialState types.Set) map[string]EnvironmentVariableFile {
+	initialVariables := make([]EnvironmentVariableFile, 0, len(initialState.Elements()))
+	initialState.ElementsAs(ctx, &initialVariables, false)
+	variableMapByKey := make(map[string]EnvironmentVariableFile, len(initialVariables))
+	for _, currentVariable := range initialVariables {
+		variableMapByKey[currentVariable.Key.ValueString()] = currentVariable
+	}
+	return variableMapByKey
+}
+
+func convertDomainVariableToEnvironmentVariableFile(v variable.Variable, variableInState *EnvironmentVariableFile) EnvironmentVariableFile {
+	description := FromString(v.Description)
+	if variableInState != nil && variableInState.Description.IsNull() {
+		description = basetypes.NewStringNull()
+	}
+	return EnvironmentVariableFile{
+		Id:          FromString(v.ID.String()),
+		Key:         FromString(v.Key),
+		Value:       FromString(v.Value),
+		MountPath:   FromString(v.MountPath),
+		Description: description,
+	}
+}
+
+func fromEnvironmentVariableFileList(ctx context.Context, initialState types.Set, vars []*qovery.EnvironmentVariable, scope qovery.APIVariableScopeEnum, variableType string) EnvironmentVariableFileList {
+	variableMap := buildVariableFileMap(ctx, initialState)
+
+	list := make([]EnvironmentVariableFile, 0, len(vars))
+	for _, v := range vars {
+		if v.Scope != scope || string(v.VariableType) != variableType {
+			continue
+		}
+		currentVariable := variableMap[v.Key]
+		description := FromNullableString(v.Description)
+		if currentVariable.Description.IsNull() && !initialState.IsNull() {
+			description = basetypes.NewStringNull()
+		}
+		mountPath := ""
+		if v.MountPath.IsSet() && v.MountPath.Get() != nil {
+			mountPath = *v.MountPath.Get()
+		}
+		list = append(list, EnvironmentVariableFile{
+			Id:          FromString(v.Id),
+			Key:         FromString(v.Key),
+			Value:       FromStringPointer(v.Value),
+			MountPath:   FromString(mountPath),
+			Description: description,
+		})
+	}
+
+	// Return nil only if list is empty and original state is nil
+	if len(list) == 0 && initialState.IsNull() {
+		return nil
+	}
+	// Otherwise return the list, even empty (`[]` in the terraform file)
+	return list
+}
---
+++ qovery/secret_file_model.go
@@ -0,0 +1,311 @@
+package qovery
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/qovery/qovery-client-go"
+
+	"github.com/qovery/terraform-provider-qovery/client"
+	"github.com/qovery/terraform-provider-qovery/internal/domain/secret"
+	"github.com/qovery/terraform-provider-qovery/internal/domain/variable"
+)
+
+var secretFileAttrTypes = map[string]attr.Type{
+	"id":          types.StringType,
+	"key":         types.StringType,
+	"value":       types.StringType,
+	"mount_path":  types.StringType,
+	"description": types.StringType,
+}
+
+type SecretFileList []SecretFile
+
+func (ss SecretFileList) toTerraformSet(ctx context.Context) types.Set {
+	var secretFileObjectType = types.ObjectType{
+		AttrTypes: secretFileAttrTypes,
+	}
+	if ss == nil {
+		return types.SetNull(secretFileObjectType)
+	}
+
+	var elements = make([]attr.Value, 0, len(ss))
+	for _, v := range ss {
+		elements = append(elements, v.toTerraformObject())
+	}
+
+	set, diagnostics := types.SetValueFrom(ctx, secretFileObjectType, elements)
+	if diagnostics.HasError() {
+		panic("TODO")
+	}
+	return set
+}
+
+func (ss SecretFileList) contains(e SecretFile) bool {
+	for _, v := range ss {
+		if e.Key == v.Key {
+			return true
+		}
+	}
+	return false
+}
+
+func (ss SecretFileList) find(key string) *SecretFile {
+	for _, v := range ss {
+		if ToString(v.Key) == key {
+			return &v
+		}
+	}
+	return nil
+}
+
+func (ss SecretFileList) diffRequest(old SecretFileList) secret.DiffRequest {
+	diff := secret.DiffRequest{
+		Create: []secret.DiffCreateRequest{},
+		Update: []secret.DiffUpdateRequest{},
+		Delete: []secret.DiffDeleteRequest{},
+	}
+
+	for _, s := range old {
+		if updatedVar := ss.find(ToString(s.Key)); updatedVar != nil {
+			if updatedVar.MountPath != s.MountPath {
+				// mount_path changed — delete + recreate
+				diff.Delete = append(diff.Delete, s.toDiffDeleteRequest())
+				diff.Create = append(diff.Create, updatedVar.toDiffCreateRequest())
+			} else if updatedVar.Value != s.Value || updatedVar.Description != s.Description {
+				diff.Update = append(diff.Update, s.toDiffUpdateRequest(*updatedVar))
+			}
+		} else {
+			diff.Delete = append(diff.Delete, s.toDiffDeleteRequest())
+		}
+	}
+
+	for _, s := range ss {
+		if !old.contains(s) {
+			diff.Create = append(diff.Create, s.toDiffCreateRequest())
+		}
+	}
+
+	return diff
+}
+
+func (ss SecretFileList) diff(old SecretFileList) client.SecretsDiff {
+	diff := client.SecretsDiff{
+		Create: []client.SecretCreateRequest{},
+		Update: []client.SecretUpdateRequest{},
+		Delete: []client.SecretDeleteRequest{},
+	}
+
+	for _, s := range old {
+		if updatedVar := ss.find(ToString(s.Key)); updatedVar != nil {
+			if updatedVar.MountPath != s.MountPath {
+				// mount_path changed — delete + recreate
+				diff.Delete = append(diff.Delete, client.SecretDeleteRequest{Id: ToString(s.Id)})
+				diff.Create = append(diff.Create, updatedVar.toCreateRequest())
+			} else if updatedVar.Value != s.Value || updatedVar.Description != s.Description {
+				diff.Update = append(diff.Update, s.toUpdateRequest(*updatedVar))
+			}
+		} else {
+			diff.Delete = append(diff.Delete, client.SecretDeleteRequest{Id: ToString(s.Id)})
+		}
+	}
+
+	for _, s := range ss {
+		if !old.contains(s) {
+			diff.Create = append(diff.Create, s.toCreateRequest())
+		}
+	}
+
+	return diff
+}
+
+type SecretFile struct {
+	Id          types.String `tfsdk:"id"`
+	Key         types.String `tfsdk:"key"`
+	Value       types.String `tfsdk:"value"`
+	MountPath   types.String `tfsdk:"mount_path"`
+	Description types.String `tfsdk:"description"`
+}
+
+func (s SecretFile) toTerraformObject() types.Object {
+	var attributes = map[string]attr.Value{
+		"id":          s.Id,
+		"key":         s.Key,
+		"value":       s.Value,
+		"mount_path":  s.MountPath,
+		"description": s.Description,
+	}
+	terraformObjectValue, diagnostics := types.ObjectValue(secretFileAttrTypes, attributes)
+	if diagnostics.HasError() {
+		panic("TODO")
+	}
+	return terraformObjectValue
+}
+
+func (s SecretFile) toCreateRequest() client.SecretCreateRequest {
+	req := client.SecretCreateRequest{
+		SecretRequest: qovery.SecretRequest{
+			Key:         ToString(s.Key),
+			Value:       ToStringPointer(s.Value),
+			Description: *qovery.NewNullableString(ToStringPointer(s.Description)),
+		},
+	}
+	mountPath := ToString(s.MountPath)
+	if mountPath != "" {
+		req.SecretRequest.MountPath = *qovery.NewNullableString(&mountPath)
+	}
+	return req
+}
+
+func (s SecretFile) toDiffCreateRequest() secret.DiffCreateRequest {
+	return secret.DiffCreateRequest{
+		UpsertRequest: secret.UpsertRequest{
+			Key:         ToString(s.Key),
+			Value:       ToString(s.Value),
+			Description: ToString(s.Description),
+			MountPath:   ToString(s.MountPath),
+		},
+	}
+}
+
+func (s SecretFile) toUpdateRequest(new SecretFile) client.SecretUpdateRequest {
+	// SecretEditRequest does NOT have MountPath — only set key/value/description
+	return client.SecretUpdateRequest{
+		Id: ToString(s.Id),
+		SecretEditRequest: qovery.SecretEditRequest{
+			Key:         ToString(s.Key),
+			Value:       ToStringPointer(new.Value),
+			Description: *qovery.NewNullableString(ToStringPointer(new.Description)),
+		},
+	}
+}
+
+func (s SecretFile) toDiffUpdateRequest(new SecretFile) secret.DiffUpdateRequest {
+	return secret.DiffUpdateRequest{
+		SecretID: ToString(s.Id),
+		UpsertRequest: secret.UpsertRequest{
+			Key:         ToString(s.Key),
+			Value:       ToString(new.Value),
+			Description: ToString(new.Description),
+			MountPath:   ToString(s.MountPath),
+		},
+	}
+}
+
+func (s SecretFile) toDiffDeleteRequest() secret.DiffDeleteRequest {
+	return secret.DiffDeleteRequest{
+		SecretID: ToString(s.Id),
+	}
+}
+
+func toSecretFile(v types.Object) SecretFile {
+	return SecretFile{
+		Id:          v.Attributes()["id"].(types.String),
+		Key:         v.Attributes()["key"].(types.String),
+		Value:       v.Attributes()["value"].(types.String),
+		MountPath:   v.Attributes()["mount_path"].(types.String),
+		Description: v.Attributes()["description"].(types.String),
+	}
+}
+
+func toSecretFileList(vars types.Set) SecretFileList {
+	if vars.IsNull() || vars.IsUnknown() {
+		return nil
+	}
+
+	secretFiles := make([]SecretFile, 0, len(vars.Elements()))
+	for _, elem := range vars.Elements() {
+		secretFiles = append(secretFiles, toSecretFile(elem.(types.Object)))
+	}
+
+	return secretFiles
+}
+
+func convertDomainSecretsToSecretFileList(initialState types.Set, secrets secret.Secrets, scope variable.Scope, variableType string) SecretFileList {
+	stateByKey := buildSecretFileMap(initialState)
+
+	list := make([]SecretFile, 0, len(secrets))
+	for _, s := range secrets {
+		if s.Scope != scope || s.Type != variableType {
+			continue
+		}
+		state := stateByKey[s.Key]
+		list = append(list, convertDomainSecretToSecretFile(s, &state))
+	}
+
+	// Return nil only if list is empty and original state list is nil
+	if len(list) == 0 && initialState.IsNull() {
+		return nil
+	}
+	// Otherwise return the list, even empty (`[]` in the terraform file)
+	return list
+}
+
+func convertDomainSecretToSecretFile(s secret.Secret, state *SecretFile) SecretFile {
+	sec := SecretFile{
+		Id:          FromString(s.ID.String()),
+		Key:         FromString(s.Key),
+		Description: FromString(s.Description),
+	}
+	if state != nil {
+		// Preserve Value from state (Secret API doesn't return values)
+		sec.Value = state.Value
+		if state.Description.IsNull() {
+			sec.Description = basetypes.NewStringNull()
+		}
+		// CRITICAL: Preserve MountPath from state when domain model has empty MountPath
+		// The legacy Secret API used by Container/Job/Environment/Project doesn't return mount_path
+		if s.MountPath != "" {
+			sec.MountPath = FromString(s.MountPath)
+		} else {
+			sec.MountPath = state.MountPath
+		}
+	} else {
+		sec.MountPath = FromString(s.MountPath)
+	}
+	return sec
+}
+
+func buildSecretFileMap(initialState types.Set) map[string]SecretFile {
+	stateList := toSecretFileList(initialState)
+	stateByKey := make(map[string]SecretFile, len(stateList))
+	for _, s := range stateList {
+		stateByKey[ToString(s.Key)] = s
+	}
+	return stateByKey
+}
+
+func fromSecretFileList(initialState types.Set, secrets []*qovery.Secret, scope qovery.APIVariableScopeEnum, secretType string) SecretFileList {
+	stateByKey := buildSecretFileMap(initialState)
+
+	list := make([]SecretFile, 0, len(secrets))
+	for _, s := range secrets {
+		if s.Scope != scope || string(*s.VariableType) != secretType {
+			continue
+		}
+		state := stateByKey[s.GetKey()]
+		sec := SecretFile{
+			Id:          FromString(s.Id),
+			Key:         FromString(s.Key),
+			Description: FromNullableString(s.Description),
+		}
+		// Preserve Value from state (Secret API doesn't return values)
+		sec.Value = state.Value
+		if state.Description.IsNull() && !initialState.IsNull() {
+			sec.Description = basetypes.NewStringNull()
+		}
+		// CRITICAL: qovery.Secret does NOT have MountPath — must come from state
+		sec.MountPath = state.MountPath
+
+		list = append(list, sec)
+	}
+
+	// Return nil only if list is empty and original state list is nil
+	if len(list) == 0 && initialState.IsNull() {
+		return nil
+	}
+	// Otherwise return the list, even empty (`[]` in the terraform file)
+	return list
+}
